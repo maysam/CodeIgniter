622096f3cd8e782c8b5f69ceaea4829b367beef3
Maysam Torabi <kherad@gmail.com>
2026-04-14 10:46:14 +0200

    add docker entrypoint with database wait logic, environment-based database config, mysql client installation, and mimes config file

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,20 @@ FROM php:7.4-apache
 
 WORKDIR /var/www/html
 
-RUN docker-php-ext-install mysqli
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends default-mysql-client && \
+    rm -rf /var/lib/apt/lists/* && \
+    docker-php-ext-install mysqli
 
 COPY . /var/www/html
+COPY docker/entrypoint.sh /usr/local/bin/app-entrypoint
+
+RUN chmod +x /usr/local/bin/app-entrypoint
 
 RUN a2enmod rewrite && \
     printf '<Directory /var/www/html>\nAllowOverride All\nRequire all granted\n</Directory>\n' > /etc/apache2/conf-available/codeigniter.conf && \
     a2enconf codeigniter
+
+ENTRYPOINT ["app-entrypoint"]
 
 EXPOSE 80
--- application/config/database.php
+++ application/config/database.php
@@ -3,10 +3,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql';
-$db['default']['username'] = 'ci_user';
-$db['default']['password'] = 'ci_password';
-$db['default']['database'] = 'ci_weblog';
+$db['default']['hostname'] = getenv('DB_HOST') ? getenv('DB_HOST') : 'mysql';
+$db['default']['username'] = getenv('DB_USER') ? getenv('DB_USER') : 'ci_user';
+$db['default']['password'] = getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : 'ci_password';
+$db['default']['database'] = getenv('DB_NAME') ? getenv('DB_NAME') : 'ci_weblog';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
